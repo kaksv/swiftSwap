d15dfe04c283630de7cfb60be3e3c53c5cbafa19
Kakooza Vianey <35223697+kaksv@users.noreply.github.com>
2025-03-07 13:02:11 +0300

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    // Initialize contract with 1 million tokens minted to the creator of the contract
+    constructor() ERC20("Wrapped Electroneum", "WETH") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
